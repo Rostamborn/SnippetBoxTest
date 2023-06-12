f5e7a0180a3dd8c114747743f6e7db66ee7384c5
Arman <rostamborne@gmail.com>
2023-06-12 21:47:56 +0330

    added loggers and imlemented dpendecy injection. added flags for configuraion

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -8,7 +8,12 @@ import (
 	"strconv"
 )
 
-func home(w http.ResponseWriter, r *http.Request) {
+type application struct {
+    errorLog *log.Logger
+    infoLog *log.Logger
+}
+
+func (app *application) home(w http.ResponseWriter, r *http.Request) {
     if r.URL.Path != "/" {
         http.NotFound(w, r)
         return
@@ -21,28 +26,29 @@ func home(w http.ResponseWriter, r *http.Request) {
     }
     ts, err := template.ParseFiles(files...)
     if err != nil {
-        log.Println(err.Error())
+        app.errorLog.Println(err.Error())
         http.Error(w, "Internal Server Error", 500)
         return
     }
 
     err = ts.Execute(w, nil)
     if err != nil {
-        log.Println(err.Error())
+        app.errorLog.Println(err.Error())
         http.Error(w, "Internal Server Error", 500)
     }
 }
 
-func showSnippet(w http.ResponseWriter, r *http.Request) {
+func (app *application) showSnippet(w http.ResponseWriter, r *http.Request) {
     id, err := strconv.Atoi(r.URL.Query().Get("id"))
     if err != nil || id < 1 {
+        app.errorLog.Println(err.Error())
         http.NotFound(w, r)
         return
     }
     fmt.Fprintf(w, "displaying item: %d", id)
 }
 
-func createSnippet(w http.ResponseWriter, r *http.Request) {
+func (app *application) createSnippet(w http.ResponseWriter, r *http.Request) {
     if r.Method != "POST" {
         w.Header().Set("Allow", "POST")
         http.Error(w, "Method Not Allowed", 405)
--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,22 +1,44 @@
 package main
 
 import (
-    "log"
-    "net/http"
+	"flag"
+	"log"
+	"net/http"
+	"os"
 )
 
 func main() {
+    addr := flag.String("addr", ":8000", "HTTP network address")
+    mess := flag.String("message", "maaaaate", "show message")
+    flag.Parse()
+
+    infoLogger := log.New(os.Stdout, "INFO: ", log.LUTC | log.Ltime | log.Ldate)
+    errLogger := log.New(os.Stderr, "ERROR\t", log.Lshortfile | log.LUTC | log.Ltime)
+
+    app := &application{
+        errorLog: errLogger,
+        infoLog: infoLogger,
+    }
+
     mux := http.NewServeMux()
-    mux.HandleFunc("/", home)
-    mux.HandleFunc("/snippet", showSnippet)
-    mux.HandleFunc("/snippet/create", createSnippet)
+    mux.HandleFunc("/", app.home)
+    mux.HandleFunc("/snippet", app.showSnippet)
+    mux.HandleFunc("/snippet/create", app.createSnippet)
 
     fileServer := http.FileServer(http.Dir("./ui/static/"))
     mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
-    log.Println("starting server on port :8000")
-    err := http.ListenAndServe(":8000", mux)
+
+    infoLogger.Printf("starting server on address %s\n", *addr)
+    infoLogger.Println("message: ", *mess)
+
+    srv := http.Server{
+        Addr: *addr,
+        ErrorLog: errLogger,
+        Handler: mux,
+    }
+    err := srv.ListenAndServe()
     if err != nil {
-        log.Fatal(err)
+        errLogger.Fatal(err)
     }
 }
