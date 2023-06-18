8ed435ccf5c819172c6e545cb69964df6f3716f9
Arman <rostamborne@gmail.com>
2023-06-18 17:03:23 +0330

    not rendering create snippet page bug, fixed

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -48,7 +48,9 @@ func (app *application) showSnippet(w http.ResponseWriter, r *http.Request) {
 }
 
 func (app *application) createSnippetForm(w http.ResponseWriter, r *http.Request) {
-    app.render(w, r, "create.page.tmpl", nil)
+    app.render(w, r, "create.page.tmpl", &templateData{
+        Form: forms.New(nil),
+    })
 }
 
 func (app *application) createSnippet(w http.ResponseWriter, r *http.Request) {
@@ -98,7 +100,7 @@ func (app *application) createSnippet(w http.ResponseWriter, r *http.Request) {
         app.render(w, r, "create.page.tmpl", &templateData{Form: form})
     }
 
-    id, err := app.snippets.Insert(form.Get("title"), form.Get("content"), "expires")
+    id, err := app.snippets.Insert(form.Get("title"), form.Get("content"), form.Get("expires"))
     if err != nil {
         app.serveError(w, err)
         return
